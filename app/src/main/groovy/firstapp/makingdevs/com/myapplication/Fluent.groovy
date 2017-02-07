075bd6c3059ce27cf98055ef662362df4a880046
carlogilmar <carlogilmar12@gmail.com>
2017-02-06 22:14:34 -0600

    Implementando llamada rest a un servicio web

---
+++ app/src/main/groovy/firstapp/makingdevs/com/myapplication/Fluent.groovy
@@ -0,0 +1,62 @@
+package firstapp.makingdevs.com.myapplication
+
+import android.os.AsyncTask
+
+public class Fluent<Result, Progress> extends AsyncTask<Object, Progress, Result> {
+    private final Closure<Result> request;
+    private final ResultConsumer<Result> then;
+    private final Closure progress;
+
+    private Fluent(Closure<Result> request, ResultConsumer<Result> then, Closure progress) {
+        this.request = request;
+        this.then = then;
+        this.progress = progress;
+    }
+
+
+    @Override
+    protected Result doInBackground(Object... params) {
+        return request.call();
+    }
+
+    @Override
+    protected void onPostExecute(Result result) {
+        then.consume(result);
+    }
+
+    @Override
+    protected void onProgressUpdate(Progress... values) {
+        if (progress != null) {
+            progress.call(values);
+        }
+    }
+
+    public static interface ResultConsumer<T> {
+        void consume(T result);
+    }
+
+    public static class FluentAsyncTaskBuilder<Result, Progress> {
+        Closure<Result> request;
+        Closure progress;
+
+        private FluentAsyncTaskBuilder<Result, Progress> from(Closure<Result> request) {
+            this.request = request;
+            return this;
+        }
+
+        FluentAsyncTaskBuilder<Result, ?> onProgress(Closure progress) {
+            this.progress = progress;
+            return this;
+        }
+
+        void then(ResultConsumer<Result> then) {
+            Fluent<Result, Progress> resultFluent = new Fluent<Result, Progress>(request, then, progress);
+            resultFluent.execute();
+        }
+    }
+
+    public
+    static <Result, Progress> FluentAsyncTaskBuilder<Result, Progress> async(Closure<Result> request) {
+        return new FluentAsyncTaskBuilder<Result, Progress>().from(request);
+    }
+}
--- app/src/main/groovy/firstapp/makingdevs/com/myapplication/MainActivity.groovy
+++ app/src/main/groovy/firstapp/makingdevs/com/myapplication/MainActivity.groovy
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import groovy.json.*
+import groovy.transform.TypeChecked
 
 class MainActivity extends AppCompatActivity {
 
@@ -20,9 +21,26 @@ class MainActivity extends AppCompatActivity {
         mButton = (Button) findViewById(R.id.button)
         println "Hola grovy------>"*100
 
-        mButton.onClickListener={
-            url= new URL("http://emailerv2.modulusuno.com/countTotal").text
-            mText.setText(url)
+        mButton.onClickListener= {
+            //url= new URL("http://emailerv2.modulusuno.com/countTotal").text
+            //mText.setText(url)
+
+
+            Fluent.async {
+                URLConnection connection
+                connection = new URL("http://emailerv2.modulusuno.com/countTotal").openConnection()
+                connection.requestMethod = 'GET'
+                connection.doOutput = true
+                def writer = new OutputStreamWriter(connection.outputStream)
+                writer.flush()
+                writer.close()
+                connection.connect()
+                println connection
+                def response = connection.content.text
+                println response
+            } then { result ->
+                println result.properties
+            }
         }
     }
 }
